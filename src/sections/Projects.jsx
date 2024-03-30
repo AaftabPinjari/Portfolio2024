c82fe5fded67b72c823de0f6c7fbf74d69832e2e
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2024-03-30 14:06:13 +0530

    addedd filmie project

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -15,16 +15,16 @@ const projects = [
     },
     {
         id: 2,
-        title: "20 React Projects",
-        src: "https://i.ibb.co/GsZp25k/20reactprojects.png",
-        alt: "20reactprojects",
-        tech: ["React", "TailwindCSS", "React-Router"],
-        description: "20 React projects built to enhance my ReactJs skills as a Front End Developer.",
-        live: "https://20-react-projects.netlify.app/",
-        code: "https://github.com/AaftabPinjari/25ReactProjects"
+        title: "Filmie",
+        src: "https://i.ibb.co/HNjn23m/filmie.png",
+        alt: "filmie",
+        tech: ["React", "Context API", "Local Storage", "OMDB API", "TailwindCSS", "React-Router"],
+        description: "A movie and Series search website built upon the OMDB API using Context API for state management and using local storage to save users watchlist Information  ",
+        live: "https://filmie-app.netlify.app/",
+        code: "https://github.com/AaftabPinjari/MovieMaze"
     },
     {
-        id: 2,
+        id: 3,
         title: "Easy Shop",
         src: "https://i.ibb.co/Vvdnz4p/easyShop.png",
         alt: "easyShop",
@@ -32,6 +32,16 @@ const projects = [
         description: "An E-commerce React Website built to enhance my skills on redux-toolkit and advanced state management in a react web App.",
         live: "https://easy-shop-app.netlify.app/",
         code: "https://github.com/AaftabPinjari/Easy-Shop"
+    },
+    {
+        id: 4,
+        title: "20 React Projects",
+        src: "https://i.ibb.co/GsZp25k/20reactprojects.png",
+        alt: "20reactprojects",
+        tech: ["React", "TailwindCSS", "React-Router"],
+        description: "20 React projects built to enhance my ReactJs skills as a Front End Developer.",
+        live: "https://20-react-projects.netlify.app/",
+        code: "https://github.com/AaftabPinjari/25ReactProjects"
     },
 ]
 const Projects = () => {
